6858217e1b1fb03df6bbe57d437f0472865c5878
klesleybr <128435353+klesleybr@users.noreply.github.com>
2023-06-07 19:01:38 -0300

    Add files via upload

---
+++ js/scripts_index.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+	$("#frases ul").bxSlider({
+		auto: true, //modificadores da função, ver Options.
+		speed: 400,
+		pager: true, //Oculta a paginação.
+        slideWidth: 800,
+	});
+  });
